59be0e2f8e214adb9bfc77520550fca7a7aa271b
Le Mong Thuy <thuyle2000@gmail.com>
2023-09-04 21:50:30 +0700

    upload

--- 7.JP1/jp1_d06_interface/src/demo/TestCasio.java
+++ 7.JP1/jp1_d06_interface/src/demo/TestCasio.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class TestCasio {
 
     public static void main(String[] args) {
+        TestCasio tc = new TestCasio();
+        tc.menu();
 
     }
 
@@ -16,28 +18,28 @@ public class TestCasio {
 
     private void input() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("nhap so thu 1: ");
+        System.out.print("nhap so thu 1: ");
         a = Double.parseDouble(sc.nextLine().trim());
-        System.out.println("nhap so thu 2: ");
+        System.out.print("nhap so thu 2: ");
         b = Double.parseDouble(sc.nextLine().trim());
     }
 
     private void inputN() {
         Scanner sc = new Scanner(System.in);
-        while(true){
-            System.out.println("nhap so nguyen [1-20]: ");
+        while (true) {
+            System.out.print("nhap so nguyen [1-20]: ");
             n = Integer.parseInt(sc.nextLine().trim());
-            if (n>=1 && n<=20){
+            if (n >= 1 && n <= 20) {
                 break;
             }
         }
     }
-    
-    private void menu(){
+
+    private void menu() {
         Casio ca = new Casio();
         Scanner sc = new Scanner(System.in);
-        String op ="";
-        while (true){
+        String op = "";
+        while (true) {
             System.out.println("\n === MY CASIO ===");
             System.out.println("1. cong 2 so");
             System.out.println("2. tru 2 so");
@@ -47,18 +49,25 @@ public class TestCasio {
             System.out.println("6. ket thuc");
             System.out.print("\n chon chuc nang [1-6]: ");
             op = sc.nextLine().trim();
-            switch (op) {
-                case "1":
-                    input();
-                    System.out.printf("\n %.2f + %.2f = %.2f \n", a, b, ca.add(a, b));
-                    break;
-                case "2": break;
-                case "3": break;
-                case "4": break;
-                case "5": inputN();
+            if (op.equals("6")) {
+                return;
+            } else if (op.equals("5")) {
+                inputN();
                 System.out.printf("\n %d! =%.0f", n, ca.factorial(n));
-                    break;
-                case "6": return;
+            } else {
+                input();
+                switch (op) {
+                    case "1" ->
+                        System.out.printf("\n %.2f + %.2f = %.2f \n", a, b, ca.add(a, b));
+                    case "2" ->
+                        System.out.printf("\n %.2f - %.2f = %.2f \n", a, b, ca.sub(a, b));
+                    case "3" ->
+                        System.out.printf("\n %.2f * %.2f = %.2f \n", a, b, ca.mul(a, b));
+                    case "4" ->
+                        System.out.printf("\n %.2f / %.2f = %.2f \n", a, b, ca.div(a, b));
+                    default ->
+                        System.out.println("\n Chon sai ma chuc nang !");
+                }
             }
         }
     }
